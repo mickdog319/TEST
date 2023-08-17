f0a0a78b6fd046681f109194707c0135ba20342f
Ed Murphy <ed@murphywebsite.com>
2023-08-17 13:20:28 -0400

    1 1

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
        placeholder="Enter City..."
        onChange={e => setCity(e.target.value)}
        value={city}
-       onKeyPress={getWeather}
+       KeyPress={getWeather}
        />
 
      {typeof weatherData.main === 'undefined' ? (
