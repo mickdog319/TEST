6b3fa3117a0aef393077650970bcf548351bfbb1
Ed Murphy <ed@murphywebsite.com>
2023-08-18 22:37:13 -0400

    1 1

--- src/App.js
+++ src/App.js
@@ -7,32 +7,86 @@ function App() {
   const MyComponent = (props) => (<p onClick={props.click}>Click Me</p>)
   
   const apiKey = '4f25502ce67ce21c020ffe855e56ed5b'
-  const [weatherData, setWeatherData] = useState([{}])
-  const [city, setCity] = useState("")
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+  
+  const [stock, setStock] = useState("");  
+  const [price, setPrice] = useState("")
+  
+  let openstock; 
 
-  const getWeather = (event) => {
-    if (event.key === "Enter")
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`)
-      .then(response => response.json()) 
-      .then(data => {
-        setWeatherData(data)
-      setCity("")
-     }
+
+
+
+  function fetchStock(){
+    const pointerToThis = this;
+    console.log(pointerToThis);
+    const API_KEY = 'T9YF0ECIYKXDFK4V';
+    const StockSymbol = stock;
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+    let stockChartXValuesFunction = [];
+    let stockChartYValuesFunction = [];
+    fetch(API_Call)
+    .then(
+        function(response) {
+            return response.json();
+        }
+            .then(data => {
+
+              //  was set to stock, setStock
+              price(data)
+            setPrice("")
+            })
+          
+          
     )
-  }
-  //const click = (event) => {
-    //alert('clicked')
-  //}
+
+    .then(
+        function(data) {
+            
+            
+            var keys = data['Time Series (Daily)'];
+
+           
+            
+            for (var key in data['Time Series (Daily)']) {
+              openstock = data['Time Series (Daily)'][key]['1. open'];
+              break;
+            }
+            alert(openstock);
+            setStock("");
+              
+        }
+    )
+
+    
+}
+
+
+
+
+
+  
   return (
     <div className="container">
-              
+        <div>      
        <input 
        className="input" 
        placeholder="Enter City, Click the 'Click Me'..."
        onChange={e => setCity(e.target.value)}
        value={city}
        ></input>
+
        
+       <input 
+       className="inputStock" 
+       placeholder="Enter Stock, Click the 'Click Me'..."
+       onChange={f => setStock(f.target.value)}
+       value={stock}
+       ></input>
+        </div>
+
+
 
        <MyComponent click={() => 
         
@@ -41,7 +95,7 @@ function App() {
         .then(data => {
           setWeatherData(data)
         setCity("")
-       })} />
+       }).then(fetchStock)} />
 
 
      {typeof weatherData.main === 'undefined' ? (
@@ -56,22 +110,26 @@ function App() {
       <p className='city'>{weatherData.name}</p>
       <p className='temp'>{Math.round(weatherData.main.temp)}°F</p>
       <p className='weather'>{weatherData.weather[0].main}</p>
+    
+      
+      
     </div>
+    
   )}
+    
+      <div className='stock-data'> 
+            <p className='stock'>{stock}</p>
+            <p className='price'>{openstock}</p>
+
+      </div>
+    
+
 </div>
+
+
   )}
     
 
-  // From Stocks App.js
-
-  function SApp() {
-    return (
-      <div className="SApp">
-        <Stock></Stock>
-        
-      </div>
-    );
-  }
   
 
 export default App
