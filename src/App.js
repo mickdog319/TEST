400742c4027d98925695d25a6937b14d61e679c4
mickdog319 <142750367+mickdog319@users.noreply.github.com>
2023-08-21 17:24:14 -0400

    1 1

--- src/App.js
+++ src/App.js
@@ -8,34 +8,29 @@ function App() {
   const apiKey = '4f25502ce67ce21c020ffe855e56ed5b'
   const [weatherData, setWeatherData] = useState([{}]);
   const [city, setCity] = useState("");
-  
   const [stock, setStock] = useState("");  
   const [openstock, setOpenstock] = useState("")
 
-  function fetchStock(){
-    const API_KEY = 'T9YF0ECIYKXDFK4V';
-    const StockSymbol = stock;
-    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+function fetchStock(){
+  const API_KEY = 'T9YF0ECIYKXDFK4V';
+  let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&outputsize=compact&apikey=${API_KEY}`;
     fetch(API_Call)
     .then(
         function(response) {
             return response.json();
-        }
-            )
-  .then(
+        })
+    .then(
         function(data) {
-            
             for (var key in data['Time Series (Daily)']) {
                setOpenstock( data['Time Series (Daily)'][key]['1. open']);
-             // break;
+               break;
             }
-        }
-    )
-}
+        })
+    }
 
   return (
     <div className="container">
-        <div>      
+    <div>      
        <input 
        className="input" 
        placeholder="Enter City Here..."
@@ -49,45 +44,33 @@ function App() {
        onChange={f => setStock(f.target.value)}
        value={stock}
        ></input>
-        </div>
+    </div>
 
        <MyComponent click={() => 
-        
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`)
         .then(response => response.json()) 
-        .then(data => {
-          setWeatherData(data)
-        setCity("")
-       }).then(fetchStock())
-      
-      } />
+        .then(data => {setWeatherData(data)})
+        .then(fetchStock())
+       } />
        
      {typeof weatherData.main === 'undefined' ? (
-      <div>
-        <p> Welcome To Weather Stock Picker! Type in any City, try a northern city(cold) and later try a southern city(hot) for the best results.</p>
-        <p> Type in any stock and remember, Check Your Spelling!  Just a few suggestions: IBM, MSFT, AMZN, and AAPL.  </p>
-         </div>
+    <div>
+      <p> Welcome To Weather Stock Picker! Type in any City, try a northern city(cold) and later try a southern city(hot) for the best results.</p>
+      <p> Type in any stock and remember, Check Your Spelling!  Just a few suggestions: IBM, MSFT, AMZN, and AAPL.  </p>
+    </div>
 
 ):(
     <>
-    <p className='weather-data'>
-
+      <p className='weather-data'>
       <p className='city'>{weatherData.name}</p>
       <p className='temp'>{Math.round(weatherData.main.temp)}°F </p>
-      <p className='weather'>{weatherData.weather[0].main}</p>
-    
-    </p>
-    <p className='stock-data'> 
-     <p className='stock'>{stock}</p>
-     <p className='openstock'>${openstock}</p>
-
-     <p className='prediction'>{weatherData.main.temp >= 85 ? "Your stock prediction: Stocks are HOT! Buy!" : "Your stock prediction: Stocks are freezing, HOLD!"}</p> 
-
-</p></>
+      <p className='weather'>{weatherData.weather[0].main}</p></p>
+      <p className='stock-data'> 
+      <p className='stock'>{stock}</p>
+      <p className='openstock'>${openstock}</p>
+      <p className='prediction'>{weatherData.main.temp >= 85 ? "Your stock prediction: Stocks are HOT! Buy!" : "Your stock prediction: Stocks are freezing, HOLD!"}</p> 
+      </p></>
   )}
- 
-
-     
-</div>
+    </div>
   )}
 export default App
